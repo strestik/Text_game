3dbc8027ba2e9adb52f9f406b07e7ee018fb202e
strstik <matejstrestik@gmail.com>
2025-05-30 14:06:19 +0200

    Big Update ( Citation needed)

--- main_Game.py
+++ main_Game.py
@@ -2,13 +2,12 @@ import random
 import time
 import sys
 alive = True
-#  hp, stamina, defense, skill, respect, abilities
 
 class Character:
     def __init__(self, name, char_class,):
         self.is_alive = True
         self.name = name
-        self.char_class = ["Monster", "Sorccer", "Archerer", "Witcher", "Jarl", "Bard"][char_class]
+        self.char_class = char_class
         self.hp = 100
         self.stamina = 100
         self.mana = 0
@@ -38,114 +37,15 @@ class Character:
             "fire bomb": False,
             }
 
-    def witcher(self):
-        self.hp = 150
-        self.stamina = 100
-        self.mana = 40
-        self.defense = 80
-        self.skill = 1.5  # dmg multiplier
-        self.char_class = "Witcher"
-        self.abilities = ["Silver Sword", "Steel Sword"]
-        self.equip = []
-        # self.signs = sign()
-
-    def sorccer(self):
-        self.hp = 110
-        self.stamina = 40
-        self.mana = 100
-        self.defense = 50
-        self.skill = 1.4
-        self.char_class = "Sorccer"
-        self.abilities = ["Fireball", "Ice Spike", "Lightning Bolt"]
-        # self.signs = sign()
-
-    def archerer(self):
-        self.hp = 130
-        self.stamina = 100
-        self.mana = 0
-        self.defense = 60
-        self.skill = 1.3
-        self.char_class = "Archerer"
-        self.abilities = ["Arrow Rain", "Poison Arrow", "Explosive Arrow"]
-        
-    def jarl(self):
-        self.hp = 170
-        self.stamina = 100
-        self.mana = 0
-        self.defense = 90
-        self.skill = 1.1
-        self.char_class = "Jarl"
-        self.abilities = ["War Cry", "Shield Bash", "Battle Roar"]
-    
-    def bard(self):
-        self.hp = 120
-        self.stamina = 80
-        self.mana = 0
-        self.defense = 40
-        self.skill = 1.2
-        self.respect = 50
-        self.char_class = "Bard"
-        self.abilities = ["Song of Healing", "Inspiring Melody", "Lullaby"]
-
-    def monster(self):
-        self.hp = 160
-        self.stamina = 100
-        self.mana = 0
-        self.defense = 70
-        self.skill = 1.3
-        self.char_class = "Monster"
-        self.abilities = ["Claw Swipe", "Bite", "Roar"]
-    
-    def attack(self):
-        if self.is_alive and enemy.is_alive:
-            damage = random.randint(10, 20) * self.skill
-            enemy.hp -= damage
-            print(f"{self.name} attacks {enemy.name} for {damage:.2f} damage!")
-
-            if enemy.hp <= 0:
-                enemy.is_alive = False
-                print(f"{enemy.name} has been defeated!")
-        else:
-            print(f"{self.name} cannot attack because they are not alive.")
-
-    
-
-    def punch(self):
-        if self.is_alive and enemy.is_alive:
-            damage = random.randint(5, 10) * self.skill
-            enemy.hp -= damage
-            print(f"{self.name} punches {enemy.name} for {damage:.2f} damage!")
-            if enemy.hp <= 0:
-                enemy.is_alive = False
-                print(f"{enemy.name} has been defeated!")
-        else:
-            print(f"{self.name} cannot punch because they are not alive.")
+    def is_alive_check(self):
+        self.is_alive = False if self.hp <= 0 else True
 
 
         
-    def sign(self):
-        self.signs = {
-            "Igni": {"burning": True,
-                     "dmg": 20,},
-            "Aard": "Wind",
-            "Quen": "Shield",
-            "Axii": "Mind Control",
-            "Yrden": "Trap"
-        }
 
 
-
-    def potion(self, potion_type):
-        pass
-
-
-        #  = {
-        #     "Sorccer": ["Fireball", "Ice Spike", "Lightning Bolt"],
-        #     "Archerer": ["Arrow Rain", "Poison Arrow", "Explosive Arrow"],
-        #     
-        #     "Jarl": ["War Cry", "Shield Bash", "Battle Roar"],
-        #     "Bard": ["Song of Healing", "Inspiring Melody", "Lullaby"]
-        # }
+    # def potion(self, potion_type):
+    #     pass
 
     def nameing(self):
         name = input("-- Zadej jméno: ").strip()
@@ -168,75 +68,143 @@ class Character:
                     print("--> Neplatná volba, zkus to znovu.")
                 
 
-    def classing(self):
-        print(f"1. Witcher\n"
-          "2. Sorccer\n"
-          "3. Archerer\n"
-          "4. Jarl\n"
-          "5. Bard\n"
-          "6. Monster\n")
-        char_class = input("Zadej číslo třídy: ").strip()
 
-        if char_class.isdigit():
+    def classing(self):
+        print("1. Witcher\n"
+              "2. Sorcerer\n"
+              "3. Archer\n"
+              "4. Jarl\n"
+              "5. Bard\n"
+              "6. Monster\n")
+        while True:
+            char_class = input("Zadej číslo třídy: ").strip()
+            if not char_class.isdigit():
+                print("--> Neplatný vstup, zkus to znovu.\n")
+                continue
             char_class = int(char_class)
             if char_class == 1:
-                self.witcher()
+                return Witcher(self.name)
             elif char_class == 2:
-                self.sorccer()
+                return Sorcerer(self.name)
             elif char_class == 3:
-                self.archerer()
+                return Archer(self.name)
             elif char_class == 4:
-                self.jarl()
+                return Jarl(self.name)
             elif char_class == 5:
-                self.bard()
+                return Bard(self.name)
             elif char_class == 6:
-                self.monster()
+                return Monster(self.name)
             else:
                 print("--> Neplatná třída, zkus to znovu.\n")
-                return self.classing()
-        else:
-            print("--> Neplatný vstup, zkus to znovu.\n")
-            return self.classing()
-        
-        
 
+
+class Witcher(Character):
+    def __init__(self, name):
+        super().__init__(name, "Witcher")
+        self.hp = 150
+        self.stamina = 100
+        self.mana = 40
+        self.defense = 80
+        self.skill = 1.5
+        self.abilities = ["Silver Sword", "Steel Sword"]
+
+    def sign(self):
+            self.signs = {
+                "Igni": {"burning": True,
+                        "dmg": 20,},
+                "Aard": "Wind",
+                "Quen": "Shield",
+                "Axii": "Mind Control",
+                "Yrden": "Trap"
+            }
+
+class Sorcerer(Character):
+    def __init__(self, name):
+        super().__init__(name, "Sorcerer")
+        self.hp = 110
+        self.stamina = 40
+        self.mana = 100
+        self.defense = 50
+        self.skill = 1.4
+        self.abilities = ["Fireball", "Ice Spike", "Lightning Bolt"]
+
+class Archer(Character):
+    def __init__(self, name):
+        super().__init__(name, "Archer")
+        self.hp = 130
+        self.stamina = 100
+        self.mana = 0
+        self.defense = 60
+        self.skill = 1.3
+        self.abilities = ["Arrow Rain", "Poison Arrow", "Explosive Arrow"]
+
+class Jarl(Character):
+    def __init__(self, name):
+        super().__init__(name, "Jarl")
+        self.hp = 170
+        self.stamina = 100
+        self.mana = 0
+        self.defense = 90
+        self.skill = 1.1
+        self.abilities = ["War Cry", "Shield Bash", "Battle Roar"]
+
+class Bard(Character):
+    def __init__(self, name):
+        super().__init__(name, "Bard")
+        self.hp = 120
+        self.stamina = 80
+        self.mana = 0
+        self.defense = 40
+        self.skill = 1.2
+        self.respect = 50
+        self.abilities = ["Song of Healing", "Inspiring Melody", "Lullaby"]
+
+class Monster(Character):
+    def __init__(self, name):
+        super().__init__(name, "Monster")
+        self.hp = 160
+        self.stamina = 100
+        self.mana = 0
+        self.defense = 70
+        self.skill = 1.3
+        self.abilities = ["Claw Swipe", "Bite", "Roar"]
  
+
+
+
+
+
 # Hero.witcher()  # Initialize as Witcher
-jm2 = Character("Crach Ain Craine", 4)
-jm3 = Character("Marigold", 5)
-jm4 = Character("Topivec", 0)
-jm4 = Character("Triss", 1)
-jm5 = Character("Jestřáb", 2)
 
-enemy = Character("Monster", 0)  # Example enemy character
+# enemy = Character()  # Example enemy character
 
 
-# print(Hero.name, Hero.char_class, Hero.hp, Hero.stamina, Hero.mana, Hero.defense, Hero.skill, Hero.respect)
-# print(jm2.name, jm2.char_class, jm2.hp, jm2.stamina, jm2.mana, jm2.defense, jm2.skill, jm2.respect)
-# print(jm3.name, jm3.char_class, jm3.hp, jm3.stamina, jm3.mana, jm3.defense, jm3.skill, jm3.respect)
-# print(jm4.name, jm4.char_class, jm4.hp, jm4.stamina, jm4.mana, jm4.defense, jm4.skill, jm4.respect)
-# print(jm5.name, jm5.char_class, jm5.hp, jm5.stamina, jm5.mana, jm5.defense, jm5.skill, jm5.respect)
 
-Hero = Character("Hero", 0)  # Initialize Hero character
-Hero.witcher()
-# print(f"\nVýtej hráč, vyber si svou postavu:")
-# Hero.nameing()
-# print("\n--> Hmm", end='', flush=True)
-# for _ in range(4):
-#     time.sleep(0.3)
-#     print('.', end='', flush=True)
-# print(f" {Hero.name}, to je dobré jméno! Vítej!!!\n")
-# time.sleep(3.5)
-# print(f"Vyber si svou třídu:\n")
-# Hero.classing()
-print(f"\n---> Tvoje postava je {Hero.name}, třída: {Hero.char_class}, HP: {Hero.hp}, Stamina: {Hero.stamina}, Mana: {Hero.mana}, Defense: {Hero.defense}, Skill: {Hero.skill}, Respect: {Hero.respect}\n")
-# time.sleep(1)
-# print(f"\nNyní můžeš začít hrát. Tvým úkolem je přežít a porazit nepřítele.\n") 
-# time.sleep(2)
-# print("Boj začínáš bez vybavení a vylepšení, jestli chceš použít meč nebo vypít lektvar, musíš na to využít kolo.\n")
-# time.sleep(2)
-# print("Ale neměj strách, protivník se připravuje, takže první tři kola na tebe nezaútočí.")
-# time.sleep(2)
+
+# Nameing
+print(f"\nVýtej hráč, vyber si svou postavu:")
+Hero = Character("Hero", "")  # Initialize Hero character
+Hero.nameing()
+print("\n--> Hmm", end='', flush=True)
+for _ in range(4):
+    time.sleep(0.3)
+    print('.', end='', flush=True)
+print(f" {Hero.name}, to je dobré jméno! Vítej!!!\n")
+time.sleep(3.5)
+
+
+
+# Classing
+print(f"Vyber si svou třídu:\n")
+Hero = Hero.classing()
+print(f"\n---> Tvoje postava je {Hero.name}, třída: {Hero.char_class}, HP: {Hero.hp}, Stamina: {Hero.stamina}, Mana: {Hero.mana}, Defense: {Hero.defense}\n")
+time.sleep(2)
+print(f"\nNyní můžeš začít hrát. Tvým úkolem je přežít a porazit nepřítele.\n") 
+time.sleep(3)
+print("Boj začínáš bez vybavení a vylepšení, jestli chceš použít meč nebo vypít lektvar, musíš na to využít kolo.\n")
+time.sleep(3)
+print("Ale neměj strách, protivník se připravuje, takže první tři kola na tebe nezaútočí.")
+time.sleep(3)
 
 
 
