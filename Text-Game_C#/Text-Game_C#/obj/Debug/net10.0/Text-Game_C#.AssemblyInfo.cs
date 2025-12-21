771836857c6e0794bcaba7b79e69d68cac35e2a7
strestik <matejstrestik@gmail.com>
2025-12-22 00:24:19 +0100

    starting

--- Text-Game_C#/Text-Game_C#/obj/Debug/net10.0/Text-Game_C#.AssemblyInfo.cs
+++ Text-Game_C#/Text-Game_C#/obj/Debug/net10.0/Text-Game_C#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Text-Game_C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07c5fff66fea420796bbd36eadfee55a247ed82")]
 [assembly: System.Reflection.AssemblyProductAttribute("Text-Game_C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Text-Game_C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
