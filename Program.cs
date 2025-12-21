32589435eb4946e48117d1ca01f926388e73ad58
strestik <matejstrestik@gmail.com>
2025-12-22 00:04:31 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Text_Game_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
